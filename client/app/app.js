c678501e854ec83123441599718046ba91f8f900
Michael Mills <rizz1337@gmail.com>
2017-02-25 12:47:52 -0500

    Add example of ng-repeat directive from $scope

--- client/app/app.js
+++ client/app/app.js
@@ -4,6 +4,19 @@ myPTApp.controller('GoalController', ['$scope', function($scope){
 
 	$scope.message = 'Here is your goal';
 
-	$scope.goals = ['finish this app', 'add it to resume', 'get a job'];
+	$scope.plan = [
+		{
+			name:'finish this app', 
+			progress:2
+		},
+		{
+			name:'add it to resume', 
+			progress:2
+		},
+		{
+			name:'get a job', 
+			progress:2
+		}
+	];
 
 }]);
