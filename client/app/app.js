6640487c23ddd9e884073ffd4ebf65f93c966f0d
Michael Mills <rizz1337@gmail.com>
2017-02-25 12:16:29 -0500

    Change GoalController params -> indexes of array

--- client/app/app.js
+++ client/app/app.js
@@ -1,9 +1,9 @@
 var myPTApp = angular.module('myPTApp', []);
 
-myPTApp.controller('GoalController', function($scope){
+myPTApp.controller('GoalController', ['$scope', function($scope){
 
 	$scope.message = 'Here is your goal';
 
 	$scope.goals = ['finish this app', 'add it to resume', 'get a job'];
 
-});
+}]);
