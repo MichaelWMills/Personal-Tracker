0119d90894065fdb350d8fb5f3003fab83575fca
Michael Mills <rizz1337@gmail.com>
2017-02-25 12:00:05 -0500

    Add few comments

--- client/app/app.js
+++ client/app/app.js
@@ -1,1 +1,13 @@
 var myPTApp = angular.module('myPTApp', []);
+
+myPTApp.config(function() {
+	//Runs before the application runs
+});
+
+myPTApp.run(function(){
+	//Executes while application runs
+});
+
+myPTApp.controller(function(){
+
+});
